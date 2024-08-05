4001013496ed1ea231151cab057e1023adf86b86
ArnavSinha24 <arnavsinha.sunny@gmail.com>
2024-08-06 01:13:36 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fugit nobis nesciunt, pariatur iusto, natus porro ab placeat libero sit qui. Autem, qui et dolorum, animi quasi ducimus quaerat voluptatum debitis asperiores obcaecati possimus!</p>
+        </div>
+    )
+}
+
+export default About
